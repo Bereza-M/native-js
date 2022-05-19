fdc880689db044f07ef05f7dbaf727e7511ba124
MichailBereznev <97397066+Bereza-M@users.noreply.github.com>
2022-05-20 00:22:34 +0300

    20.05

---
+++ src/03/03.ts
@@ -0,0 +1,47 @@
+import {StudentType} from "../02/02";
+import {GovernmentBuildingsType, HouseType} from "../02/02_02";
+
+export const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+function sum2(a: number, b: number) {
+    return a + b;
+}
+
+export const addSkill = (student: StudentType, skill: string) => {
+    student.technologies.push({
+        id: new Date().getTime(),
+        title: skill
+    })
+}
+
+function addSkill2(student: StudentType, skill: string) {
+    student.technologies.push({
+        id: new Date().getTime(),
+        title: skill
+    })
+}
+
+export function makeStudentActive(s: StudentType) {
+    s.isActive = true;
+}
+export const doesStudentLiveIn = (s: StudentType, cityName: string) => {
+    return s.address.city.title === cityName;
+}
+
+export const addMoneyToBudget = (building: GovernmentBuildingsType, budget: number) => {
+    building.budget += budget;
+};
+
+export const repairHouse = (houseType: HouseType) =>{
+    houseType.repaired = true;
+}
+
+export const toFireStaff = (building: GovernmentBuildingsType, staffCountToFire: number) => {
+    building.staffCount -= staffCountToFire;
+}
+
+export const toHireStaff = (building: GovernmentBuildingsType, staffCountToHire: number) => {
+    building.staffCount += staffCountToHire;
+}
